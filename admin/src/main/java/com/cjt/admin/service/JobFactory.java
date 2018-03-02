c06a8d820eb7dd0a545c0cc0e863c2118e8cb160
caojiantao <caojiantao30@gmail.com>
2018-03-02 14:10:48 +0800

    完善JobFactory，采用注入ApplicationContext方式；

--- admin/src/main/java/com/cjt/admin/service/JobFactory.java
+++ admin/src/main/java/com/cjt/admin/service/JobFactory.java
@@ -2,6 +2,7 @@ package com.cjt.admin.service;
 
 import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.quartz.AdaptableJobFactory;
@@ -10,17 +11,13 @@ import org.springframework.scheduling.quartz.AdaptableJobFactory;
  * @author caojiantao
  * @since 2018-02-13 19:59:48
  */
-public class JobFactory extends AdaptableJobFactory implements ApplicationContextAware {
+public class JobFactory extends AdaptableJobFactory {
 
+    @Autowired
     private ApplicationContext context;
 
     @Override
     protected Object createJobInstance(TriggerFiredBundle bundle) {
         return context.getBean(bundle.getJobDetail().getJobClass());
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.context = applicationContext;
     }
 }
