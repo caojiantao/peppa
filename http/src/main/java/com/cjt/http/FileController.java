611b6cc1afb193a203c8b94ff731163c6c3c6b78
caojiantao <caojiantao30@gmail.com>
2018-04-03 15:21:04 +0800

    强制下载增加response设定

--- http/src/main/java/com/cjt/http/FileController.java
+++ http/src/main/java/com/cjt/http/FileController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedInputStream;
 import java.io.IOException;
