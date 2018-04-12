9f1574a232b4d1ca42d453a0d4767f81fd90418e
caojiantao <caojiantao30@gmail.com>
2018-04-12 11:22:39 +0800

    增加http工具类，十分满意哈哈哈

--- common/src/main/java/com/cjt/common/util/HttpUtils.java
+++ common/src/main/java/com/cjt/common/util/HttpUtils.java
@@ -1,11 +1,8 @@
 package com.cjt.common.util;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.Proxy;
-import java.net.URL;
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -14,23 +11,164 @@ import java.util.Set;
  */
 public class HttpUtils {
 
-    public static InputStream getInputStream(String url, Map<String, String> requestProperty, long timeout, Proxy proxy) throws IOException {
-        InputStream is = null;
-        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection(proxy);
-        connection.addRequestProperty("Referer", url);
-        connection.connect();
-        return is;
+    private final static String UTF_8 = "utf-8";
+
+    private final static String GET = "GET";
+
+    private final static String POST = "POST";
+
+    private final static String COOKIE = "Cookie";
+
+    private final static String USER_AGENT = "User-Agent";
+
+    private final static String REFERER = "Referer";
+
+    private final static String LINE_SEPARATOR = "line.separator";
+
+    private String url;
+
+    private Map<String, String> headers;
+
+    private Map<String, String> cookies;
+
+    private Map<String, String> data;
+
+    private int timeout;
+
+    private String method;
+
+    private Proxy proxy;
+
+    private Callback callback;
+
+    public static HttpUtils connect(String url) {
+        HttpUtils utils = new HttpUtils();
+        utils.url = url;
+        utils.headers = new HashMap<>(1 << 3);
+        utils.cookies = new HashMap<>(1 << 3);
+        utils.data = new HashMap<>(1 << 3);
+        utils.timeout = 6000;
+        utils.method = "GET";
+        utils.proxy = null;
+        utils.callback = null;
+        return utils;
     }
 
-    private static String toCookies(Map<String, String> map) {
-        Set<Map.Entry<String, String>> entries = map.entrySet();
+    public HttpUtils header(String key, String value) {
+        headers.put(key, value);
+        return this;
+    }
+
+    public HttpUtils cookie(String key, String value) {
+        cookies.put(key, value);
+        return this;
+    }
+
+    public HttpUtils data(String key, String value) {
+        data.put(key, value);
+        return this;
+    }
+
+    public HttpUtils userAgent(String value) {
+        headers.put(USER_AGENT, value);
+        return this;
+    }
+
+    public HttpUtils referer(String value) {
+        headers.put(REFERER, value);
+        return this;
+    }
+
+    public HttpUtils proxy(String ip, int port) {
+        SocketAddress address = new InetSocketAddress(ip, port);
+        proxy = new Proxy(Proxy.Type.HTTP, address);
+        return this;
+    }
+
+    public HttpUtils callback(Callback callback) {
+        this.callback = callback;
+        return this;
+    }
+
+    public InputStream execute() throws IOException {
+        if (callback != null) {
+            new Thread(() -> {
+                try {
+                    InputStream is = doHttp();
+                    callback.handle(is);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+            return null;
+        } else {
+            return doHttp();
+        }
+    }
+
+    private InputStream doHttp() throws IOException {
+        url += GET.equalsIgnoreCase(method) ? getParamStr(data) : "";
+        HttpURLConnection connection = (proxy == null) ? (HttpURLConnection) new URL(url).openConnection() : (HttpURLConnection) new URL(url).openConnection();
+        connection.setConnectTimeout(timeout);
+        connection.setRequestMethod(method);
+        headers.forEach(connection::addRequestProperty);
+        connection.addRequestProperty(COOKIE, getCookieStr(cookies));
+        if (POST.equalsIgnoreCase(method)) {
+            connection.setDoOutput(true);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), UTF_8));
+            writer.write(getParamStr(data));
+            writer.flush();
+            writer.close();
+        }
+        return connection.getInputStream();
+    }
+
+    public InputStream get() throws IOException {
+        this.method = GET;
+        return execute();
+    }
+
+    public InputStream post() throws IOException {
+        this.method = POST;
+        return execute();
+    }
+
+    private String getParamStr(Map<String, String> data) {
+        if (!data.isEmpty()) {
+            StringBuilder builder = new StringBuilder();
+            Set<Map.Entry<String, String>> entries = data.entrySet();
+            builder.append("?");
+            data.forEach((key, value) -> builder.append(key).append("=").append(value).append("&"));
+            return builder.substring(0, builder.length() - 1);
+        }
+        return "";
+    }
+
+    private String getCookieStr(Map<String, String> cookies) {
         StringBuilder builder = new StringBuilder();
-        entries.forEach(entry -> {
-            builder.append(entry.getKey());
-            builder.append("=");
-            builder.append(entry.getValue());
-            builder.append(";");
-        });
+        cookies.forEach((key, value) -> builder.append(key).append("=").append(value).append(";"));
         return builder.toString();
+    }
+
+    public static String getStringFromInputStream(InputStream is) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder builder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+            builder.append(System.getProperty(LINE_SEPARATOR, "\n"));
+        }
+        return builder.toString();
+    }
+
+    public interface Callback {
+
+        /**
+         * 请求回调处理
+         *
+         * @param is 输入流
+         * @throws IOException 请求异常
+         */
+        void handle(InputStream is) throws IOException;
     }
 }
--- http/src/main/java/com/cjt/http/FileController.java
+++ http/src/main/java/com/cjt/http/FileController.java
@@ -1,6 +1,6 @@
 package com.cjt.http;
 
-import com.cjt.common.util.StreamUtils;
+import com.cjt.common.util.HttpUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +23,7 @@ public class FileController {
     public void getTsInputStream(String src, HttpServletResponse response) {
         // 强制下载
         response.setContentType("application/force-download");
-        try (InputStream is = StreamUtils.getInputStream(URLDecoder.decode(src, "UTF-8"));
+        try (InputStream is = HttpUtils.connect(URLDecoder.decode(src, "UTF-8")).execute();
              BufferedInputStream bis = new BufferedInputStream(is);
              OutputStream os = response.getOutputStream()) {
             byte[] buffers = new byte[1024 * 1024];
