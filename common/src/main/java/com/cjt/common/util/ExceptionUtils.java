7274dc35c533173cae2134c3535066db4f61feed
caojiantao <caojiantao30@gmail.com>
2018-04-26 16:35:15 +0800

    完善异常转换工具类

--- common/src/main/java/com/cjt/common/util/ExceptionUtils.java
+++ common/src/main/java/com/cjt/common/util/ExceptionUtils.java
@@ -1,11 +1,12 @@
 package com.cjt.common.util;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 /**
  * @author caojiantao
- * @since  2017-08-11 00:37:25
+ * @since 2017-08-11 00:37:25
  */
 public class ExceptionUtils {
 
@@ -13,11 +14,13 @@ public class ExceptionUtils {
      * 异常转换为详情字符串
      */
     public static String toDetailStr(Exception ex) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw, true);
-        ex.printStackTrace(pw);
-        pw.flush();
-        sw.flush();
-        return sw.toString();
+        try (StringWriter sw = new StringWriter();
+             PrintWriter pw = new PrintWriter(sw, true)) {
+            ex.printStackTrace(pw);
+            return sw.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
